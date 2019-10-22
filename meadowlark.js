4fca1cb3cbe55ad4ac26510bd5c2fae10ddfd272
unknown <shalmayev1993@gmail.com>
2019-10-22 09:35:48 +0300

    First commit

---
+++ meadowlark.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+
+app.get('/', (req, res, next) => {
+    res.send('hello');
+});
+
+// custom 404 page
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('404 - Not Found');
+});
+
+// custom 500 page
+app.use(function (err, req, res, next) {
+    console.error(err.stack);
+    res.type('text/plain');
+    res.status(500);
+    res.send('500 - Server Error');
+});
+
+app.listen(app.get('port'), function () {
+    console.log('Express started on http://localhost:' +
+        app.get('port') + '; press Ctrl-C to terminate.');
+});
