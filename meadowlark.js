3847ad426c2397e21344b500b289408d1a05374e
unknown <shalmayev1993@gmail.com>
2019-10-22 13:45:06 +0300

    update1

--- meadowlark.js
+++ meadowlark.js
@@ -1,11 +1,43 @@
+require('dotenv').config()
 var express = require('express');
 var app = express();
+var expressLayouts = require('express-ejs-layouts');
 
 app.set('port', process.env.PORT || 3000);
 
+/**************************** Serving Static Files ****************************/
+app.use(express.static('public'));
 
+/**************************** Local Variables ****************************/
+app.use((req,res,next)=>{
+    res.locals.url = process.env.BASE_URL;
+    res.locals.logo = process.env.LOGO;
+    next();
+});
+/**************************** EJS ****************************/
+app.set('view engine', 'ejs');
+app.use(expressLayouts);
+
+/**************************** Routing ****************************/
+
+// Home Page
 app.get('/', (req, res, next) => {
-    res.send('hello');
+    res.render('pages/index');
+});
+
+// About Page
+app.get('/about', (req,res,next)=>{
+    let fortunes = [
+        "Conquer your fears or they will conquer you.",
+        "Rivers need springs.",
+        "Do not fear what you don't know.",
+        "You will have a pleasant surprise.",
+        "Whenever possible, keep it simple.",
+       ];
+    let randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
+    res.render('pages/about',{
+        fortunes: randomFortune,
+    });
 });
 
 // custom 404 page
